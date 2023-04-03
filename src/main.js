b53440d3e8376aa5e97cf035009d9813c7d247af
HuangJianbin <907310116@qq.com>
2023-04-03 11:08:19 +0800

    fix:无法引入elementUI文件

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import 'element-plus/dist/index.css' // 需要引入外部elementPlus-css文件
 import router from './router'
 import store from './store'
 import "./guardPermission"
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,30 @@
-import {createStore} from "vuex"
+import { createStore } from "vuex"
+import { getInfo } from '~/api/login'
 const store = createStore({
-  state(){
+  state () {
     return {
-      
+      userInfo: {}, //用户信息
     }
   },
   // 同步
-  mutations:{
-
+  mutations: {
+    // 记录用户信息
+    SET_USERINFO (state, user) {
+      state.userInfo = user
+    },
   },
   // 异步
-  actions:{
+  actions: {
+    // 获取当前登录用户信息
+    getInfo ({ commit }) {
+      return new Promise((resolve, reject) => {
+        getInfo().then(res => {
+          commit("SET_USERINFO", res.result)
+          resolve(res)
+        }).catch(err => reject(err))
+      })
 
+    }
   }
 })
 export default store
